eb239b8c37d560f301b031949cf928696ef54992
sebasMartinez11 <smartinez2009@hotmail.com>
2025-09-12 20:44:39 -0300

    esto para la ruta de propiedad

--- crminm/urls.py
+++ crminm/urls.py
@@ -3,10 +3,14 @@ from usuarios.views import ListaYCreaUsuario, DetalleUsuario
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from leads.views import EstadoLeadViewSet, ContactoViewSet
+from propiedades.views import  PropiedadViewSet
+
+
 
 router = DefaultRouter()
 router.register(r"estados-lead", EstadoLeadViewSet)
 router.register(r"contactos", ContactoViewSet)
+router.register(r"propiedades", PropiedadViewSet)
 
 urlpatterns = [
     path("api/", include(router.urls)),
@@ -15,6 +19,10 @@ urlpatterns = [
     path('api/usuarios/', ListaYCreaUsuario.as_view(), name='usuarios-lista'),
     # Para operaciones sobre un usuario específico
     path('api/usuarios/<int:pk>/', DetalleUsuario.as_view(), name='usuario-detalle'),
+
 ]
+
+
+
 
 
